6c857ee6c54ce1713c72e4dfcf2fd94cfb6c328c
jducuara42 <60761835+jducuara42@users.noreply.github.com>
2020-02-08 09:51:16 -0500

    entrega Clase #1
    entrega Clase #1

--- clase1/fibonacci.py
+++ clase1/fibonacci.py
@@ -12,14 +12,14 @@ def fibo(n):
 		i = i + 1
 	print("el factorial",n,"-esimo es: ", f1 + f2)
 
-def fibo_recur(n, indice, fib1, fib2):
+def fibo_rec(n, indice, fib1, fib2):
 	if indice < n:
 		#print(fib1, " + ",fib2, " =", fib1  + fib2 )
 		temp = fib1 
 		fib1  = fib1  + fib2 
 		fib2  = temp
 		indice  = indice  + 1
-		fibo_recur(n, indice, fib1, fib2)
+		fibo_rec(n, indice, fib1, fib2)
 	else:
 		print("Fin de la función recursiva")
 		print("el factorial",n,"-esimo es: ", fib1  + fib2 )
@@ -29,4 +29,4 @@ print(fibo(10))
 indice = 1
 fib1 = 1
 fib2 = 0
-print(fibo_recur(10, indice, fib1, fib2))
+print(fibo_rec(10, indice, fib1, fib2))
